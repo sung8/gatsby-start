0991222ff2d09fd8e3bed911b98cd4bc5ecfdd12
sung8 <heechuw@gmail.com>
2020-08-25 19:16:45 -0400

    blog posts show on blog page with graphql query

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ['gatsby-plugin-sass'],
+  siteMetadata: {
+    title: 'personal blog',
+    author: 'SC',
+  },
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
+    'gatsby-transformer-remark',
+  ],
 }
---
+++ src/posts/react.md
@@ -0,0 +1,6 @@
+---
+title: 'React'
+date: '2019-08-25'
+---
+
+In this post you'll learn React.
