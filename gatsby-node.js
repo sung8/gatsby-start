6a64ce6f66d767fa04f4516dd5694b60bf3de398
sung8 <heechuw@gmail.com>
2020-08-26 04:01:15 -0400

    deploy to contentful cms, render contentful posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,46 +1,4 @@
-// gatsby node config file
-/*
-  - allows tap in to the node APIs that gatsby expoeses
-  - will use this config for slugs and generating 
-*/
 const path = require('path')
-// our function runs when a new node is created
-// const { createNode, createNodeField } = actions
-module.exports.onCreateNode = ({ node, actions }) => {
-  // ⭐
-  const { createNodeField } = actions
-
-  // this if statement filters to only log our blog post data
-  if (node.internal.type === 'MarkdownRemark') {
-    const slug = path.basename(node.fileAbsolutePath, '.md')
-    // what we care when we log is "internal"
-    //console.log(JSON.stringify(node, undefined, 4))
-    // we will tap into "fileAbsolutePath" on the output
-    // E:/gatsbyjs-tutorial/my-project/gatsby-start/src/posts/gatsby.md
-    /* node.js documentation
-      path.basename(path[, ext]) method (see ⭐)
-        - reduces file path to just the file name and extension
-        - we can also just get the file name
-    */
-    // @@@ makes it more visible in the console
-    // outputs 'react' and 'gatsby' which is what we want
-    //console.log('@@@@@@@@@@@@@@', slug)
-
-    /* call createNodeField to actually add the new field onto the node
-      - call an object w/ 
-        - 1. node that we're trying to create the node field on, 
-        - 2. set the name for our new field, 
-        - 3. the value for the new field
-     */
-    // goal 1 finished
-    createNodeField({
-      // this is shorthand for 'node = node'
-      node,
-      name: 'slug',
-      value: slug,
-    })
-  }
-}
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -52,12 +10,10 @@ module.exports.createPages = async ({ graphql, actions }) => {
   // 2. Get markdown data
   const res = await graphql(`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
@@ -66,13 +22,13 @@ module.exports.createPages = async ({ graphql, actions }) => {
 
   // iterate over posts and run the createPage for each of them
   // 3. Create new pages
-  res.data.allMarkdownRemark.edges.forEach(edge => {
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       // context contains stuff we can pass down to the template
       context: {
-        slug: edge.node.fields.slug,
+        slug: edge.node.slug,
       },
     })
   })
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,25 +4,20 @@ import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 import blogStyles from './blog.module.scss'
 /*
-1. Fetch the slug for posts
-2. Use slug to generate a link to the post page
+1. Swap out the markdown query with the contentful query
+2. Update the component to render the new data
+    - Don't worry if the link links to a non-existent page
 3. Test work
 */
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            html
-            excerpt
-            fields {
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
@@ -33,15 +28,13 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>
-      {/* edges is an array */}
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map(edge => {
+        {data.allContentfulBlogPost.edges.map(edge => {
           return (
             <li className={blogStyles.post}>
-              <Link to={`/blog/${edge.node.fields.slug}`}>
-                <h2>{edge.node.frontmatter.title}</h2>
-                <p>{edge.node.frontmatter.date}</p>
+              <Link to={`/blog/${edge.node.slug}`}>
+                <h2>{edge.node.title}</h2>
+                <p>{edge.node.publishedDate}</p>
               </Link>
             </li>
           )
